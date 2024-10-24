57be3db19d3d805eccecf2dad8ec62375f3b636b
Hossein-H-I <167168985+Hossein-H-I@users.noreply.github.com>
2024-10-24 19:20:07 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let signupPage = document.querySelector('#signup-page');
+let form = document.querySelector('#myForm');
+let errorMessage = document.querySelector('#error-message');
+let emailInput = document.querySelector('#email');
+let submitBtn = document.querySelector('#submitBtn');
+
+let thanksPage = document.querySelector('#thanks-page');
+let dismessageBtn = document.querySelector('#dis-messageBtn');
+
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const emailValue = emailInput.value.trim(); // email value without space around it
+
+    if(emailValue == '' || !emailPattern.test(emailValue)){
+        emailInput.classList.add('error-input');
+        errorMessage.textContent= 'Valid email required';
+    } else{
+        signupPage.style.display = 'none';
+        thanksPage.style.display = 'flex';
+        
+        // show user email in thansks message
+        document.getElementById('user-email').textContent = emailValue;
+
+    }
+
+})
+
+dismessageBtn.addEventListener('click',function(){
+    thanksPage.style.display = 'none';
+    signupPage.style.cssText = 'flex';
+    form.reset();
+})
